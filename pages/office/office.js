3c27dcd291d189f6f3fed4bef96ece77585ee371
coder board <1019463979@qq.com>
2017-12-03 14:00:18 +0800

    UI update

--- pages/office/office.js
+++ pages/office/office.js
@@ -1,3 +1,37 @@
 Page({
-	
+	data:{
+		rank:[
+               {
+                rating:'25%',
+                office:'阿里',
+                number:25
+               },
+               {
+               	rating:'25%',
+               	office:'百度'
+               },
+               {
+               	rating:'20%',
+               	office:'腾讯'
+               }
+		],
+		percentGoes:[
+               {
+               	number:250,
+               	office:'其他'
+               },
+               {
+                number:100,
+                office:'腾讯',
+               },
+               {
+               	number:100,
+               	office:'阿里'
+               },
+               {
+               	number:50,
+               	office:'百度'
+               }
+		]
+	}
 })
--- pages/person/person.js
+++ pages/person/person.js
@@ -6,16 +6,6 @@ Page({
             office:'12th CEO',
             department:'研发部'
          },
-       nav:[
-          {
-            message:'编辑个人信息',
-            url:'../../pages/person-edit/person-edit'
-          },
-          {
-          	message:'设置',
-          	url:'../../pages/person-set/person-set'
-          }
-       ]
 	},
    
 })
